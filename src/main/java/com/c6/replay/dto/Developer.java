0e03f317c1a04514c82fa52275c9bbd5df4a6473
Marccudi <marccudi@hotmail.es>
2022-08-19 11:05:29 +0200

    clases marc creadas

---
+++ src/main/java/com/c6/replay/dto/Developer.java
@@ -0,0 +1,67 @@
+package com.c6.replay.dto;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+@Table(name = "developers")
+public class Developer {
+	@Id
+	private int id;
+	@Column
+	private String name;
+
+	@OneToMany
+	@JoinColumn(name = "id")
+	private List<Game> game;
+
+	public Developer() {
+	}
+
+	public Developer(int id, String name, List<Game> game) {
+		this.id = id;
+		this.name = name;
+		this.game = game;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	@JsonIgnore
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "game")
+	public List<Game> getGame() {
+		return game;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setGame(List<Game> game) {
+		this.game = game;
+	}
+
+	@Override
+	public String toString() {
+		return "Developer [id=" + id + ", name=" + name + "]";
+	}
+
+}
--- src/main/java/com/c6/replay/dto/Genre.java
+++ src/main/java/com/c6/replay/dto/Genre.java
@@ -1,5 +1,29 @@
 package com.c6.replay.dto;
 
-public class Genre {
+import java.util.List;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+@Table(name = "genres")
+public class Genre {
+	@Id
+	private int id;
+	@Column
+	private String name;
+	
+	@OneToMany
+	@JoinColumn(name = "id")
+	private List<Game> game;
+
+
+	
 }
