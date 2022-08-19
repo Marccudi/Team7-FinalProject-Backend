d88e0c8390b9b2f140401ebf858b2b4dd8c0aff8
Marccudi <marccudi@hotmail.es>
2022-08-19 12:10:25 +0200

    Marc Controllers finished

--- src/main/java/com/c6/replay/controller/DeveloperController.java
+++ src/main/java/com/c6/replay/controller/DeveloperController.java
@@ -1,5 +1,71 @@
 package com.c6.replay.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.c6.replay.dto.Developer;
+import com.c6.replay.service.DeveloperServiceImpl;
+
+@RestController
+@RequestMapping("/api")
 public class DeveloperController {
+	@Autowired
+	DeveloperServiceImpl developerServiceImpl;
+	
+	@GetMapping("/developers")
+	public List<Developer> listDevelopers() {
+		return developerServiceImpl.listDevelopers();
+	}
+	
+	@PostMapping("/developers")
+	public Developer saveDeveloper(@RequestBody Developer developer) {
+		return developerServiceImpl.saveDeveloper(developer);
+	}
+	
+	@GetMapping("/developers/{id}")
+	public Developer developerXID(@PathVariable(name="id") int id) {
+		
+		Developer developer_xid= new Developer();
+		
+		//Find developer with id
+		developer_xid=developerServiceImpl.developerXID(id);
+		
+		System.out.println("Developer by ID: "+developer_xid);
+		
+		return developer_xid;
+	}
+	
+	@PutMapping("/developers/{id}")
+	public Developer updateDeveloper(@PathVariable(name="id")int id,@RequestBody Developer developer) {
+		
+		Developer selectedDeveloper= new Developer();
+		Developer updatedDeveloper= new Developer();
+		
+		//Find existing developer with id
+		selectedDeveloper= developerServiceImpl.developerXID(id);
+		
+		//Set the values to the given parameters
+		selectedDeveloper.setName(developer.getName());
+		
+		updatedDeveloper = developerServiceImpl.updateDeveloper(selectedDeveloper);
+		
+		System.out.println("The updated developer is: "+ updatedDeveloper);
+		
+		return updatedDeveloper;
+	}
+	
+	@DeleteMapping("/developers/{id}")
+	public void deleteDeveloper(@PathVariable(name="id")int id) {
+		developerServiceImpl.deleteDeveloper(id);
+	}
 
 }
--- src/main/java/com/c6/replay/controller/GenreController.java
+++ src/main/java/com/c6/replay/controller/GenreController.java
@@ -1,5 +1,71 @@
 package com.c6.replay.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.c6.replay.dto.Genre;
+import com.c6.replay.service.GenreServiceImpl;
+
+@RestController
+@RequestMapping("/api")
 public class GenreController {
+	@Autowired
+	GenreServiceImpl genreServiceImpl;
+	
+	@GetMapping("/genres")
+	public List<Genre> listGenres() {
+		return genreServiceImpl.listGenres();
+	}
+	
+	@PostMapping("/genres")
+	public Genre saveGenre(@RequestBody Genre genre) {
+		return genreServiceImpl.saveGenre(genre);
+	}
+	
+	@GetMapping("/genres/{id}")
+	public Genre genreXID(@PathVariable(name="id") int id) {
+		
+		Genre genre_xid= new Genre();
+		
+		//Find genre with id
+		genre_xid=genreServiceImpl.genreXID(id);
+		
+		System.out.println("Genre by ID: "+genre_xid);
+		
+		return genre_xid;
+	}
+	
+	@PutMapping("/genres/{id}")
+	public Genre updateGenre(@PathVariable(name="id")int id,@RequestBody Genre genre) {
+		
+		Genre selectedGenre= new Genre();
+		Genre updatedGenre= new Genre();
+		
+		//Find existing genre with id
+		selectedGenre= genreServiceImpl.genreXID(id);
+		
+		//Set the values to the given parameters
+		selectedGenre.setName(genre.getName());
+		
+		updatedGenre = genreServiceImpl.updateGenre(selectedGenre);
+		
+		System.out.println("The updated genre is: "+ updatedGenre);
+		
+		return updatedGenre;
+	}
+	
+	@DeleteMapping("/genres/{id}")
+	public void deleteGenre(@PathVariable(name="id")int id) {
+		genreServiceImpl.deleteGenre(id);
+	}
 
 }
