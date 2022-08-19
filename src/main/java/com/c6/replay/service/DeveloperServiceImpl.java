b04168446ad4ecd754718930401345c0f5112cd9
Marccudi <marccudi@hotmail.es>
2022-08-19 11:48:55 +0200

    Marc ServiceImple finished

--- src/main/java/com/c6/replay/service/DeveloperServiceImpl.java
+++ src/main/java/com/c6/replay/service/DeveloperServiceImpl.java
@@ -2,8 +2,43 @@ package com.c6.replay.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.c6.replay.dao.IDeveloperDAO;
 import com.c6.replay.dto.Developer;
 
-public class DeveloperServiceImpl {
+public class DeveloperServiceImpl  implements IDeveloperService{
+	@Autowired
+	IDeveloperDAO developerDAO;
+	
+	@Override
+	public List<Developer> listDevelopers() {
+		// TODO Auto-generated method stub
+		return developerDAO.findAll();
+	}
+
+	@Override
+	public Developer saveDeveloper(Developer developer) {
+		// TODO Auto-generated method stub
+		return developerDAO.save(developer);
+	}
+
+	@Override
+	public Developer developerXID(int id) {
+		// TODO Auto-generated method stub
+		return developerDAO.findById(id).get();
+	}
+
+	@Override
+	public Developer updateDeveloper(Developer developer) {
+		// TODO Auto-generated method stub
+		return  developerDAO.save(developer);
+	}
+
+	@Override
+	public void deleteDeveloper(int id) {
+		// TODO Auto-generated method stub
+		developerDAO.deleteById(id);
+	}
 
 }
--- src/main/java/com/c6/replay/service/PlatformServiceImpl.java
+++ src/main/java/com/c6/replay/service/PlatformServiceImpl.java
@@ -1,5 +1,44 @@
 package com.c6.replay.service;
 
-public class PlatformServiceImpl {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.c6.replay.dao.IPlatformDAO;
+import com.c6.replay.dto.Platform;
+import com.c6.replay.dto.Platform;
+
+public class PlatformServiceImpl implements IPlatformService{
+	@Autowired
+	IPlatformDAO platformDAO;
+	
+	@Override
+	public List<Platform> listPlatforms() {
+		// TODO Auto-generated method stub
+		return platformDAO.findAll();
+	}
+
+	@Override
+	public Platform savePlatform(Platform platform) {
+		// TODO Auto-generated method stub
+		return platformDAO.save(platform);
+	}
+
+	@Override
+	public Platform platformXID(int id) {
+		// TODO Auto-generated method stub
+		return platformDAO.findById(id).get();
+	}
+
+	@Override
+	public Platform updatePlatform(Platform platform) {
+		// TODO Auto-generated method stub
+		return  platformDAO.save(platform);
+	}
+
+	@Override
+	public void deletePlatform(int id) {
+		// TODO Auto-generated method stub
+		platformDAO.deleteById(id);
+	}
 }
