e787788882c6723f92be43dc0b771a7e7c22fc0b
David Marcos <david.13254@gmail.com>
2022-08-19 12:39:58 +0200

    platforms

--- src/main/java/com/c6/replay/dto/Platform.java
+++ src/main/java/com/c6/replay/dto/Platform.java
@@ -13,7 +13,7 @@ import javax.persistence.Table;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
-@Table(name = "platform")
+@Table(name = "platforms")
 public class Platform {
 	@Id
 	private int id;
