fffb23126f5940c1ef9918af8a4d77a4b9a519f4
Jimxx22 <jaumegil22@gmail.com>
2022-08-19 11:11:03 +0200

    User

--- src/main/java/com/c6/replay/dto/Borrow.java
+++ src/main/java/com/c6/replay/dto/Borrow.java
@@ -1,5 +1,16 @@
 package com.c6.replay.dto;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="borrowings")
 public class Borrow {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id; // AutoIncremental ID
 }
