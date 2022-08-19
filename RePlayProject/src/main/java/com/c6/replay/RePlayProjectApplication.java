9fe07eea5690769c885c19ea900483c4c8363b26
Jimxx22 <jaumegil22@gmail.com>
2022-08-19 10:39:57 +0200

    project creation

---
+++ RePlayProject/src/main/java/com/c6/replay/RePlayProjectApplication.java
@@ -0,0 +1,13 @@
+package com.c6.replay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RePlayProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RePlayProjectApplication.class, args);
+	}
+
+}
---
+++ RePlayProject/src/test/java/com/c6/replay/RePlayProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.c6.replay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RePlayProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
