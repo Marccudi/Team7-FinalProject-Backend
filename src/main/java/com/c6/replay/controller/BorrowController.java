324ee72357fdf59db1a8899198fd72ef0f608dbf
Jimxx22 <jaumegil22@gmail.com>
2022-09-23 20:48:04 +0200

    aa

--- src/main/java/com/c6/replay/controller/BorrowController.java
+++ src/main/java/com/c6/replay/controller/BorrowController.java
@@ -43,7 +43,7 @@ public class BorrowController {
 	
 	@PostMapping("/borrows")
 	public Borrow saveBorrow(@RequestBody Borrow borrow) {
-		enviarConGMail(borrow.getUserOwner().getEmail(), "Peticion de prestamo", "El usuarios "+borrow.getUserBorrower().getUsername()+" pide prestado el juego "+borrow.getGame().getTitle());
+		//enviarConGMail(borrow.getUserOwner().getEmail(), "Peticion de prestamo", "El usuarios "+borrow.getUserBorrower().getUsername()+" pide prestado el juego "+borrow.getGame().getTitle());
 		return borrowServiceImpl.saveBorrow(borrow);
 	}
 	
@@ -128,33 +128,33 @@ public class BorrowController {
         return borrowServiceImpl.listBorrowByUserOwner(id);
     }
 	
-	private static void enviarConGMail(String destinatario, String asunto, String cuerpo) {
-	    // Esto es lo que va delante de @gmail.com en tu cuenta de correo. Es el remitente también.
-	    String remitente = "replay";  //Para la dirección nomcuenta@gmail.com
-
-	    Properties props = System.getProperties();
-	    props.put("mail.smtp.host", "smtp.gmail.com");  //El servidor SMTP de Google
-	    props.put("mail.smtp.user", remitente);
-	    props.put("mail.smtp.clave", "AIzaSyB53BsKeclH_NwPSpaF-iv6X4XvH4m8uig");    //La clave de la cuenta
-	    props.put("mail.smtp.auth", "true");    //Usar autenticación mediante usuario y clave
-	    props.put("mail.smtp.starttls.enable", "true"); //Para conectar de manera segura al servidor SMTP
-	    props.put("mail.smtp.port", "587"); //El puerto SMTP seguro de Google
-
-	    Session session = Session.getDefaultInstance(props);
-	    MimeMessage message = new MimeMessage(session);
-
-	    try {
-	        message.setFrom(new InternetAddress(remitente));
-	        message.addRecipients(Message.RecipientType.TO, destinatario);   //Se podrían añadir varios de la misma manera
-	        message.setSubject(asunto);
-	        message.setText(cuerpo);
-	        Transport transport = session.getTransport("smtp");
-	        transport.connect("smtp.gmail.com", remitente, "AIzaSyB53BsKeclH_NwPSpaF-iv6X4XvH4m8uig");
-	        transport.sendMessage(message, message.getAllRecipients());
-	        transport.close();
-	    }
-	    catch (MessagingException me) {
-	        me.printStackTrace();   //Si se produce un error
-	    }
-	}
+//	private static void enviarConGMail(String destinatario, String asunto, String cuerpo) {
+//	    // Esto es lo que va delante de @gmail.com en tu cuenta de correo. Es el remitente también.
+//	    String remitente = "replay";  //Para la dirección nomcuenta@gmail.com
+//
+//	    Properties props = System.getProperties();
+//	    props.put("mail.smtp.host", "smtp.gmail.com");  //El servidor SMTP de Google
+//	    props.put("mail.smtp.user", remitente);
+//	    props.put("mail.smtp.clave", "AIzaSyB53BsKeclH_NwPSpaF-iv6X4XvH4m8uig");    //La clave de la cuenta
+//	    props.put("mail.smtp.auth", "true");    //Usar autenticación mediante usuario y clave
+//	    props.put("mail.smtp.starttls.enable", "true"); //Para conectar de manera segura al servidor SMTP
+//	    props.put("mail.smtp.port", "587"); //El puerto SMTP seguro de Google
+//
+//	    Session session = Session.getDefaultInstance(props);
+//	    MimeMessage message = new MimeMessage(session);
+//
+//	    try {
+//	        message.setFrom(new InternetAddress(remitente));
+//	        message.addRecipients(Message.RecipientType.TO, destinatario);   //Se podrían añadir varios de la misma manera
+//	        message.setSubject(asunto);
+//	        message.setText(cuerpo);
+//	        Transport transport = session.getTransport("smtp");
+//	        transport.connect("smtp.gmail.com", remitente, "AIzaSyB53BsKeclH_NwPSpaF-iv6X4XvH4m8uig");
+//	        transport.sendMessage(message, message.getAllRecipients());
+//	        transport.close();
+//	    }
+//	    catch (MessagingException me) {
+//	        me.printStackTrace();   //Si se produce un error
+//	    }
+//	}
 }
