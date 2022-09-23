04e9176b51f38da3d576606e98d07038698e501e
Jimxx22 <jaumegil22@gmail.com>
2022-09-23 20:44:17 +0200

    prova mail

--- src/main/java/com/c6/replay/controller/BorrowController.java
+++ src/main/java/com/c6/replay/controller/BorrowController.java
@@ -4,6 +4,14 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -35,6 +43,7 @@ public class BorrowController {
 	
 	@PostMapping("/borrows")
 	public Borrow saveBorrow(@RequestBody Borrow borrow) {
+		enviarConGMail(borrow.getUserOwner().getEmail(), "Peticion de prestamo", "El usuarios "+borrow.getUserBorrower().getUsername()+" pide prestado el juego "+borrow.getGame().getTitle());
 		return borrowServiceImpl.saveBorrow(borrow);
 	}
 	
@@ -118,4 +127,34 @@ public class BorrowController {
     public List<Borrow> borrowsByBorrower(@PathVariable(name="id") Long id) {
         return borrowServiceImpl.listBorrowByUserOwner(id);
     }
+	
+	private static void enviarConGMail(String destinatario, String asunto, String cuerpo) {
+	    // Esto es lo que va delante de @gmail.com en tu cuenta de correo. Es el remitente también.
+	    String remitente = "replay";  //Para la dirección nomcuenta@gmail.com
+
+	    Properties props = System.getProperties();
+	    props.put("mail.smtp.host", "smtp.gmail.com");  //El servidor SMTP de Google
+	    props.put("mail.smtp.user", remitente);
+	    props.put("mail.smtp.clave", "AIzaSyB53BsKeclH_NwPSpaF-iv6X4XvH4m8uig");    //La clave de la cuenta
+	    props.put("mail.smtp.auth", "true");    //Usar autenticación mediante usuario y clave
+	    props.put("mail.smtp.starttls.enable", "true"); //Para conectar de manera segura al servidor SMTP
+	    props.put("mail.smtp.port", "587"); //El puerto SMTP seguro de Google
+
+	    Session session = Session.getDefaultInstance(props);
+	    MimeMessage message = new MimeMessage(session);
+
+	    try {
+	        message.setFrom(new InternetAddress(remitente));
+	        message.addRecipients(Message.RecipientType.TO, destinatario);   //Se podrían añadir varios de la misma manera
+	        message.setSubject(asunto);
+	        message.setText(cuerpo);
+	        Transport transport = session.getTransport("smtp");
+	        transport.connect("smtp.gmail.com", remitente, "AIzaSyB53BsKeclH_NwPSpaF-iv6X4XvH4m8uig");
+	        transport.sendMessage(message, message.getAllRecipients());
+	        transport.close();
+	    }
+	    catch (MessagingException me) {
+	        me.printStackTrace();   //Si se produce un error
+	    }
+	}
 }
